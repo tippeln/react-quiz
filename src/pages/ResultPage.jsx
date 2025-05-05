dc211c5a0c117370a264b3407fcc7b3f05a3058c
tippeln <tippeln@gmail.com>
2025-05-05 14:49:45 +0900

    fix

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ROUTES } from "../const";
+import Result from "../components/Result/Result";
+import Loading from "../components/Loading/Loading";
 
 export default function ResultPage() {
-  return <div>ResultPage</div>;
+  const [active, setActive] = useState(false);
+  const location = useLocation();
+  const maxQuizLen = location.state.maxQuizLen;
+  const correctNumLen = location.state.correctNumLen;
+
+  useEffect(() => {
+    setTimeout(() => setActive(true), 3000);
+  }, []);
+
+  return (
+    <>
+      <Loading active={active} />
+      <h1>ResultPage</h1>
+      <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
+      <br />
+      <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
+    </>
+  );
 }
