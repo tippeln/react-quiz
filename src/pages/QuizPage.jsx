c36da118fffdf8e556fd3a2bb7e5ec4ed579e2e7
tippeln <tippeln@gmail.com>
2025-05-05 13:36:45 +0900

    link  result

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,22 +1,57 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
+import { useEffect, useState } from "react";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
-  const quizIndex = 0;
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
+  console.log("MAX_QUIZ_LEN:", MAX_QUIZ_LEN);
+
   const handleClick = (clickedIndex) => {
-    console.log("clickedIndex:", clickedIndex);
+    if (clickedIndex === quizData[quizIndex].answerIndex) {
+      setAnswerLogs((prev) => [...prev, true]);
+    } else {
+      setAnswerLogs((prev) => [...prev, false]);
+    }
+    setQuizIndex((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if (answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter((answer) => {
+        return answer === true;
+      });
+      console.log("correctNum:", correctNum);
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN,
+          correctNum: correctNum,
+        },
+      });
+    }
+  }, [answerLogs, MAX_QUIZ_LEN, navigation]);
+
+  console.log("answerLogs:", answerLogs);
+
   return (
     <>
-      <Display>{`Q1.${quizData[quizIndex].question}`}</Display>
-      {quizData[quizIndex].options.map((option, index) => {
-        return (
-          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
-            {option}
-          </Button>
-        );
-      })}
+      {quizData[quizIndex] && (
+        <Display>{`Q1.${quizData[quizIndex].question}`}</Display>
+      )}
+
+      {quizData[quizIndex] &&
+        quizData[quizIndex].options.map((option, index) => {
+          return (
+            <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+              {option}
+            </Button>
+          );
+        })}
     </>
   );
 }
